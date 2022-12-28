cc6414c4033427e99f218072adcead951b80fbea
AhmedMujtaba999 <116835811+AhmedMujtaba999@users.noreply.github.com>
2022-12-29 00:47:07 +0530

    Update run()_vs_start().java

--- Thread/run()_vs_start().java
+++ Thread/run()_vs_start().java
@@ -15,13 +15,19 @@ public class Example extends Thread {
 		Example obj = new Example();
 		
 		Thread t1 = new Thread();
-		t1.start();// a new thread will be created which is responsible for running  the run() present in Thread class.
-		t1.run();// no new thread will be created we are directly calling Thread class Run() just like normal method call.		
+		t1.start();// a new thread will be created which is responsible for running  the run() 
+			   //present in Thread class.
+		t1.run();// no new thread will be created we are directly calling Thread class Run()
+		         //just like normal method call.		
+		
+		
 		
 		
 		Thread t2 =new Thread (obj);
-		t2.start();//a new thread will be created which is responsible for running the run() present in ClassA.
-		t2.run();// no new thread  will be created but as we are overriding run() present in Thread class, our ClassA run() will be executed.
+		t2.start();//a new thread will be created which is responsible for running the run()
+			   //present in ClassA.
+		t2.run();// no new thread  will be created but as we are overriding run() 
+		         //present in Thread class, our ClassA run() will be executed.
 		
 	}
 	
